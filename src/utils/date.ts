17de69e418b53b2524774203df4916a26e7818ee
Fernando <fernando@inspiration-q.com>
2025-04-24 13:06:04 +0200

    Make dates in Spanish

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,7 @@
 import { format, parseISO } from 'date-fns';
+import { es } from 'date-fns/locale';
 
 export function formatDate(date: Date | string) {
   const dateObj = typeof date === 'string' ? parseISO(date) : date;
-  return format(dateObj, 'MMMM d, yyyy');
+  return format(dateObj, "d 'de' MMMM 'de' yyyy", { locale: es });
 }
